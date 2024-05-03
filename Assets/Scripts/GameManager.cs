0b4ac5139b2903ce00d438bca8d76b299808f10f
Miguel Rico Feo <miguelrico2031@gmail.com>
2024-05-03 23:43:10 +0200

    duro comento

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,26 +19,30 @@ public class GameManager : NetworkBehaviour
 {
     [field:SerializeField] public int MaxConnections { get; private set; }
     public static GameManager Instance { get; private set; }
-    public RaceController CurrentRace { get; private set; }
+    public RaceController CurrentRace { get; private set; } = null;
 
+    //variables de red
     [HideInInspector] public NetworkVariable<int> NumPlayers;
-    public NetworkVariable<PlayerInfo> HostInfo;
+    [HideInInspector] public NetworkVariable<PlayerInfo> HostInfo;
+    [HideInInspector] public NetworkVariable<Circuit> SelectedCircuit;
     public NetworkList<PlayerInfo> PlayerInfos;
-    public NetworkVariable<Circuit> SelectedCircuit;
+    
     
     [SerializeField] private Player _playerPrefab;
-
+    
     private NetworkManager _networkManager;
+    private PlayerInfo _localPlayerInfo;
     private MainMenuUI _mainMenuUI;
-
+    private readonly HashSet<ulong> _readyPlayers = new();
     private bool _disconnectedLocally = false;
 
-    private HashSet<ulong> _readyPlayers = new();
-    
 
+    
+    
+    #region Unity Callbacks
     private void Awake()
     {
-
+        //singleton
         if (Instance != null)
         {
             Destroy(gameObject);
@@ -63,13 +67,17 @@ public class GameManager : NetworkBehaviour
         _networkManager.ConnectionApprovalCallback += ApprovalCheck;
         _mainMenuUI = FindObjectOfType<MainMenuUI>();
     }
-
+    
     private void OnDisable()
     {
 
     }
+    
+    
+    #endregion
 
-
+    
+    #region Main Menu Methods
     public async void StartHost(Action<string> sucessCallback, Action failCallback)
     {
         await UnityServices.InitializeAsync();
@@ -89,7 +97,7 @@ public class GameManager : NetworkBehaviour
         }
         catch(Exception e) // si falla llama al callback de error
         {
-            Debug.LogError(e);
+            // Debug.LogError(e);
             failCallback();
         }
     }
@@ -113,27 +121,31 @@ public class GameManager : NetworkBehaviour
         }
         catch (Exception e) //callback de fallo si falla
         {
-            Debug.LogError(e);
+            // Debug.LogError(e);
             failCallback();
         }
     }
     
-    private void ApprovalCheck(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
-    {
-        response.Approved = SceneManager.GetActiveScene().buildIndex == 0;
+    private void LoadGameScene()
+    { //carga la escena del juego
+        _networkManager.SceneManager.LoadScene("Game", LoadSceneMode.Single);
     }
-
     
-    public override void OnNetworkSpawn() 
+    #endregion
+    
+    
+    #region Main Menu Network Callbacks
+    
+    public override void OnNetworkSpawn() //se ejecuta en cada cliente o host 1 sola vez al spawnear el network object
     {
         //se obtienen los datos del jugador que se haya unido / haya creado la sala
         var id = _networkManager.LocalClientId;
         var playerName = new FixedString64Bytes(_mainMenuUI.PlayerName);
         var playerColor = _mainMenuUI.PlayerColor;
-        
+        _localPlayerInfo = new PlayerInfo(id, playerName, playerColor); //copia local para saber nuestro playerInfo
         if (IsHost)  //si es el host guardamos su info duplicada en la variable HostInfo
         {
-            HostInfo.Value = new PlayerInfo(id, playerName, playerColor);
+            HostInfo.Value = new PlayerInfo(id, playerName, playerColor); //variable de red modificada solo por el Host
             SelectedCircuit.Value = _mainMenuUI.SelectedCircuit;
             _networkManager.OnClientDisconnectCallback += OnClientDisconnected;
         }
@@ -143,52 +155,64 @@ public class GameManager : NetworkBehaviour
         base.OnNetworkSpawn();
     }
     
-    
     private void OnClientConnected(ulong id) { }
+    
+    #endregion
 
+
+    #region General Network Callbacks
+    
+    private void ApprovalCheck(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
+    { //evita que clientes se puedan unir si se excede el maximo de usuarios o ya empezo una partida
+        response.Approved = SceneManager.GetActiveScene().buildIndex == 0 && NumPlayers.Value < MaxConnections;
+    }
     private void OnClientDisconnected(ulong id)
     {
-        Debug.Log(id + "Desconectado");
+        if (!IsHost) return; //Solo se ejecuta en el host
+        //decrementa los jugadores y borra el playerInfo del cliente desconectado
         NumPlayers.Value--;
         int i;
         for (i = 0; i < PlayerInfos.Count; i++)
             if (PlayerInfos[i].ID == id) break;
         PlayerInfos.RemoveAt(i);
-        _readyPlayers.Remove(id);
+        _readyPlayers.Remove(id); //borra la peticion de iniciar partida del cliente desconectado (si la hubiera)
     }
 
-    private void OnServerStopped(bool b) //se llama local en el host cuando se para el servber
+    private void OnServerStopped(bool b) //se llama local en el host cuando se para el server
     {
-        Debug.Log($"stopeado servero y el bool es {b}");
+        Debug.Log($"server parado -> {b}");
         ReturnToMainMenu();
     }
     
     private void OnClientStopped(bool b) //se llama en el cliente local solo cuando se desconecta
     {
-        Debug.Log($"stopeado cliento y el bool es {b}");
-        if (_disconnectedLocally) ReturnToMainMenu();
-        else _mainMenuUI.ShowErrorPanel(ReturnToMainMenu);
+        Debug.Log($"cliente parado -> {b}");
+        if (_disconnectedLocally) ReturnToMainMenu(); //si se ha desconectado por voluntad propia
+        else _mainMenuUI.ShowErrorPanel(ReturnToMainMenu); //si no, se muestra una ventana de error de conexion
     }
-
-    private void ReturnToMainMenu()
-    {
-        _networkManager.OnClientConnectedCallback -= OnClientConnected;
-        _networkManager.OnServerStopped -= OnServerStopped;
-        _networkManager.OnClientStopped -= OnClientStopped;
-        SceneManager.LoadScene(0);
-    }
-
+    
+    #endregion
+    
+    
+    #region General Network Methods
     public void Disconnect()
     {
         _disconnectedLocally = true;
         _networkManager.Shutdown();
     }
-
-    private void LoadGameScene()
+    
+    private void ReturnToMainMenu()
     {
-        _networkManager.SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        _networkManager.OnClientConnectedCallback -= OnClientConnected;
+        _networkManager.OnServerStopped -= OnServerStopped;
+        _networkManager.OnClientStopped -= OnClientStopped;
+        SceneManager.LoadScene(0); //carga el menu principal (asumiendo que el networkmanager esta apagado)
     }
     
+    #endregion
+
+    
+    #region Main Menu RPCS
     [ServerRpc(RequireOwnership = false)]
     private void AddPlayerInfoServerRpc(ulong id, FixedString64Bytes playerName, PlayerInfo.PlayerColor playerColor)
     {
@@ -198,8 +222,10 @@ public class GameManager : NetworkBehaviour
 
     [ServerRpc(RequireOwnership = false)]
     public void SetPlayerReadyServerRpc(ulong id)
-    {
+    { //si los jugadores listos para jugar son la mayoria (y hay al menos 2) empieza el juego
         _readyPlayers.Add(id);
         if (NumPlayers.Value > 1 && _readyPlayers.Count > NumPlayers.Value / 2f) LoadGameScene();
     }
+    
+    #endregion
 }
